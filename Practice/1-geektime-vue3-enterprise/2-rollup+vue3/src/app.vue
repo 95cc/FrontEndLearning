2e8f50e8bc46b06ff10b670b1479a7db77c55af5
95cc <71340227+95cc@users.noreply.github.com>
2024-07-17 23:35:50 +0800

    极客时间 《Vue 3企业级项目实战课》03 rollup+vue3

---
+++ Practice/1-geektime-vue3-enterprise/2-rollup+vue3/src/app.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { reactive } from 'vue'
+const state = reactive({
+  count: 0,
+})
+const onClick = () => {
+  state.count++
+}
+</script>
+
+<template>
+  <div class="demo">
+    <div class="text">{{ state.count }}</div>
+    <button class="btn" @click="onClick">Add</button>
+  </div>
+</template>
+
+<style>
+.demo {
+  width: 200px;
+  padding: 10px;
+  box-shadow: 0px 0px 9px #00000066;
+  text-align: center;
+}
+.demo .text {
+  font-size: 28px;
+  font-weight: bolder;
+  color: #666666;
+}
+.demo .btn {
+  font-size: 20px;
+  padding: 0 10px;
+  height: 32px;
+  min-width: 80px;
+  cursor: pointer;
+}
+</style>
